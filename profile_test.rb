52728c1c1850aac7402fa0d340891d94ec05929d
Jim A. Driscoll <jimcrypt@Jim-Driscolls-Computer.local>
2007-01-19 23:34:24 +0000

    Add a new (non-CBC) profiler script

--- profile_test.rb
+++ profile_test.rb
@@ -1,14 +1,9 @@
 #!/usr/bin/env ruby -rprofile
 require ARGV[0]||"./core"
 require "./rijndael"
-require "crypt/cbc"
 
-puts "Encrypting a fairly big block of text...\n";
-
-huge_ptext = File.open("bwulf10.txt", "r").read
-big_ptext = huge_ptext[0, 10240]
-#big_ptext="Oh, the grand old Duke of York,\nHe had ten thousand men;\nHe marched them up to the top of the hill\nAnd he marched them down again."
+puts "Encrypting a block of text...\n";
 
 cipher=Crypt::Rijndael.new("1234567890abcdef")
 
-big_ctext=Crypt::CBC.new(cipher).encrypt("This is an IV...", big_ptext)
+cipher.encrypt("This is an IV...")
