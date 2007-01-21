e6743ceeb70e480ad9f492c891cbd703dbeb11c6
Jim A. Driscoll <jimcrypt@Jim-Driscolls-Computer.local>
2007-01-21 12:36:57 +0000

    Boost the number of blocks in the profile test to get better results

--- profile_cbc_test.rb
+++ profile_cbc_test.rb
@@ -6,7 +6,7 @@ require "crypt/cbc"
 puts "Encrypting a fairly big block of text...\n";
 
 huge_ptext = File.open("bwulf10.txt", "r").read
-big_ptext = huge_ptext[0, 10240]
+big_ptext = huge_ptext[0, 102400]
 #big_ptext="Oh, the grand old Duke of York,\nHe had ten thousand men;\nHe marched them up to the top of the hill\nAnd he marched them down again."
 
 cipher=Crypt::Rijndael.new("1234567890abcdef")
