0c9aeda01458915750037d4395d1d407dc135b25
Jim Driscoll <jim.a.driscoll@gmail.com>
2011-04-10 16:22:16 +0100

    Workaround for spaces in $PWD

--- mktar.sh
+++ mktar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-progname=`basename $PWD`
+progname=`basename $PWD | tail -1`
 full_vername=$progname-`git describe --tags | sed s/^v//`
 git tar-tree HEAD $full_vername | gzip -c > ../$full_vername.tar.gz
