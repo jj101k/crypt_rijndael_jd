a7a7cde34e540beb22726bcca775bc3c409a974b
Jim Driscoll <jim.a.driscoll@gmail.com>
2011-04-10 17:05:15 +0100

    Ask for 0644

--- install.rb
+++ install.rb
@@ -4,10 +4,10 @@ require "fileutils"
 include FileUtils::Verbose
 
 mkdir_p(CONFIG["sitelibdir"]+"/crypt")
-install("rijndael.rb", CONFIG["sitelibdir"]+"/crypt")
+install("rijndael.rb", CONFIG["sitelibdir"]+"/crypt", :mode=>0644)
 if(File.exists? "Makefile")
 	system((ENV["MAKE"]||"make")+' install')
 else
 	mkdir_p(CONFIG["sitelibdir"]+"/crypt/rijndael")
-	install("core.rb", CONFIG["sitelibdir"]+"/crypt/rijndael/")
+	install("core.rb", CONFIG["sitelibdir"]+"/crypt/rijndael/", :mode=>0644)
 end
